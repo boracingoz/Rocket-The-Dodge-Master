bc5eb2217b76d0d4813fcace9e2ad60da7980b7e
boracingoz26 <boracingoz@gmail.com>
2024-09-28 06:57:56 +0300

    Added new levels with corresponding level design implementation.

--- Assets/Scripts/Controller/MoverWallController.cs
+++ Assets/Scripts/Controller/MoverWallController.cs
@@ -6,10 +6,9 @@ namespace Controller
     public class MoverWallController : WallController
     {
         [SerializeField] Vector3 _direction;
-        //[Range (0,1f)]
-        [SerializeField] float _factor;
         [SerializeField] float _speed = 1f;
 
+        private  float _factor;
         private Vector3 _startPos; // walls pos 
         const float FULL_CIRCLE = Mathf.PI * 2;
 
@@ -22,9 +21,9 @@ namespace Controller
         {
             float cycle = Time.time / _speed;
             float sinWave = Mathf.Sin(cycle * FULL_CIRCLE);
-            
 
             _factor = Mathf.Abs(sinWave);
+
             Vector3 offset = _direction * _factor;
             transform.position = offset + _startPos;
 
